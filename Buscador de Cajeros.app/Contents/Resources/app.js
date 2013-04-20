d58a06c2ba4be8d2719f43cd5724755fb8be462d
Alejandro García Iglesias <ale.garciaiglesias@gmail.com>
2013-04-20 20:07:47 -0300

    Add app bundle.

---
+++ Buscador de Cajeros.app/Contents/Resources/app.js
@@ -0,0 +1,83 @@
+process.title = 'Buscador de Cajeros';
+
+var app = module.exports = require('appjs');
+
+app.serveFilesFrom(__dirname + '/content');
+
+var menubar = app.createMenu([{
+  label:'&File',
+  submenu:[
+    {
+      label:'E&xit',
+      action: function(){
+        window.close();
+      }
+    }
+  ]
+},{
+  label:'&Window',
+  submenu:[
+    {
+      label:'Fullscreen',
+      action:function(item) {
+        window.frame.fullscreen();
+        console.log(item.label+" called.");
+      }
+    },
+    {
+      label:'Minimize',
+      action:function(){
+        window.frame.minimize();
+      }
+    },
+    {
+      label:'Maximize',
+      action:function(){
+        window.frame.maximize();
+      }
+    },{
+      label:''//separator
+    },{
+      label:'Restore',
+      action:function(){
+        window.frame.restore();
+      }
+    }
+  ]
+}]);
+
+menubar.on('select',function(item){
+  console.log("menu item "+item.label+" clicked");
+});
+
+var window = app.createWindow({
+  icons  : __dirname + '/content/icons'
+});
+
+window.on('create', function(){
+  console.log("Window Created");
+  window.frame.show();
+  window.frame.focus();
+  window.frame.center();
+  window.frame.maximize();
+  window.frame.setMenuBar(menubar);
+});
+
+window.on('ready', function(){
+  console.log("Window Ready");
+  window.process = process;
+  window.module = module;
+
+  function F12(e){ return e.keyIdentifier === 'F12' }
+  function Command_Option_J(e){ return e.keyCode === 74 && e.metaKey && e.altKey }
+
+  window.addEventListener('keydown', function(e){
+    if (F12(e) || Command_Option_J(e)) {
+      window.frame.openDevTools();
+    }
+  });
+});
+
+window.on('close', function(){
+  console.log("Window Closed");
+});
